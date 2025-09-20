52e6f1079d4c037c41eec127eab5830253b28c3d
DominicDev <s0l0estudi4r@gmail.com>
2025-09-20 23:17:57 +0200

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'GLUDE';
+export let nameSongs = [
+	"BREATHE", /*1*/
+	"IDENTITIY - ORIGINAL", /*2*/
+	"DREAMERS", /*3*/
+	"IDENTITIY - NCS", /*4*/
+	"TRAVELER", /*5*/
+	"BREATHE X DREAMERS MASHUP", /*6*/
+	"IDENTITY (ORIGINAL MIX)", /*7*/
+	"SPIRIT OF THINGS", /*8*/
+	"RIGHT HERE ", /*9*/
+	"CLOSE (REMIX)", /*10*/
+	"OUT MY WAY", /*11*/
+	"JUST CUZ", /*12*/
+	"KENDRA LOVES TAPE", /*13*/
+	"CORTANA", /*14*/
+	"ROCKETSHIP", /*15*/
+	"ELON MUSK", /*16*/
+	"GIMME MY SPACE", /*17*/
+	"SOMETHING TELLS ME YES", /*18*/
+	"TRAVEL WITH ME", /*19*/
+	"IT'S GOING TO BE OKAY WITH ME", /*20*/
+]
